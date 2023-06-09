aae84400d9e3b7bc84b2af179366f9d08c56fe5b
sweet62 <maks_shuyanov@mail.ru>
2023-06-09 14:33:44 +0300

    Creating timer Page

---
+++ src/Component/molecules/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from './Header.module.scss'
+import { FlexContainer } from "../../common";
+
+export const Header = () => {
+    return (
+        <FlexContainer width={1920} className={styles.Header}>
+        </FlexContainer>
+    )
+}
